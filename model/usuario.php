da130706b3b4837beab3c561729e67be9effbdc9
Isabel Ceron <ceron.isabel.97@gmail.com>
2020-12-11 21:30:12 -0600

    empty

--- model/usuario.php
+++ model/usuario.php
@@ -11,10 +11,14 @@
       echo "Bienvenido ".$user;
 
    } else {
+    
+    if(isset($_GET["fallo"]) && $_GET["fallo"] == 'true')
+    {
+        echo "<div style='color:red'> Usuario o contraseña invalido </div>";
+    }
+    //echo "¡Usuario o contraseña incorrectos!";
 
-      echo "¡Usuario o contraseña incorrectos!";
-
-      echo '<br><a href="'.$_SERVER['HTTP_REFERER'].'">Volver</a>';
+      //echo '<br><a href="'.$_SERVER['HTTP_REFERER'].'">Volver</a>';
 
    }
 

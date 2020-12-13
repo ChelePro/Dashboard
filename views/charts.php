5356f41dd3f03963c5bcdf838044a7487c9c07d4
Ricardo Elías <ricardoportillo14@gmail.com>
2020-12-13 11:31:58 -0600

    Conexiones
    Conexiones

---
+++ views/charts.php
@@ -0,0 +1,230 @@
+<?php
+	require 'ConsultasDB.php';
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Dashboard</title>
+        <link  rel="icon"   href="assets/img/icono.png" type="image/png" />
+        <link href="css/styles.css" rel="stylesheet" />
+        <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" crossorigin="anonymous"></script>
+    </head>
+    <body class="sb-nav-fixed">
+        <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+            <a class="navbar-brand" href="index.html">Dashboard</a>
+            <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
+            <!-- Navbar Search-->
+            <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
+                <div class="input-group">
+                    
+                </div>
+            </form>
+            <!-- Navbar-->
+            
+        </nav>
+        <div id="layoutSidenav">
+            <div id="layoutSidenav_nav">
+                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                    <div class="sb-sidenav-menu">
+                        <div class="nav">
+                            <div class="sb-sidenav-menu-heading">Principal</div>
+                            <a class="nav-link" href="index.html">
+                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                Dashboard
+                            </a>
+                            
+                            <div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-parent="#sidenavAccordion">
+                                <nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
+
+                                </nav>
+                            </div>
+                            <div class="sb-sidenav-menu-heading">Opciones</div>
+                            <a class="nav-link" href="charts.html">
+                                <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
+                                Query
+                            </a>
+                            <a class="nav-link" href="tables.html">
+                                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                                Acerca de
+                            </a>
+                        </div>
+                    </div>
+                    
+                </nav>
+            </div>
+            <div id="layoutSidenav_content">
+                <main>
+                    <div class="container-fluid">
+                        <h2 class="mt-4">Sistema de Analisis de Ingresos y Egresos del Salvadore&ntildeo promedio </h2>
+                    <div class="card mb-4">
+                    </div>
+                        <div class="card mb-4">
+                            <div class="card-header">
+                                <i class="fas fa-table mr-1"></i>
+                                  Egresos totales por grupo ocupacional durante el 2017
+                            </div>
+                            <div class="card-body">
+                                <div class="table-responsive">
+                                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                           <tr>
+                                       <td>Grupo ocupacional</td>
+                                        <td>Promedio total de gastos</td>
+                                         <td>Gasto en alimentacion</td>
+                                          <td>Gasto en educacion</td>
+                                           <td>Gasto en salud</td>
+                                           <td>Gasto en empleo</td>
+                                           <td>Gasto en vivienda</td>
+                                    </tr>
+
+                                    <?php
+
+                                        foreach($Resultado1 as $mostrar)
+                                        {
+                                    ?>
+                                    <tr>
+                                        <td><?php echo $mostrar['GrupoOcupacional']; ?></td>
+                                        <td><?php echo $mostrar['Promedio_Gastos']; ?></td>
+                                        <td><?php echo $mostrar['Gasto_Alimentacion']; ?></td>
+                                        <td><?php echo $mostrar['Gasto_Educacion']; ?></td>
+                                        <td><?php echo $mostrar['Gasto_Salud']; ?></td>
+                                        <td><?php echo $mostrar['Gasto_Vivienda']; ?></td>
+                                        <td><?php echo $mostrar['Gasto_Empleo']; ?></td>
+                                    </tr>
+                                    <?php } ?>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card mb-4">
+                            <div class="card-header">
+                                <i class="fas fa-table mr-1"></i>
+                                  Egresos totales por grupo ocupacional durante el 2018
+                            </div>
+                            <div class="card-body">
+                                <div class="table-responsive">
+                                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                           <tr>
+                                       <td>Grupo ocupacional</td>
+                                        <td>Promedio total de gastos</td>
+                                         <td>Gasto en alimentacion</td>
+                                          <td>Gasto en educacion</td>
+                                           <td>Gasto en salud</td>
+                                           <td>Gasto en empleo</td>
+                                           <td>Gasto en vivienda</td>
+                                    </tr>
+
+                                    <?php
+
+                                        foreach($Resultado2 as $mostrar2)
+                                        {
+                                    ?>
+                                    <tr>
+                                        <td><?php echo $mostrar2['GrupoOcupacional']; ?></td>
+                                        <td><?php echo $mostrar2['Promedio_Gastos']; ?></td>
+                                        <td><?php echo $mostrar2['Gasto_Alimentacion']; ?></td>
+                                        <td><?php echo $mostrar2['Gasto_Educacion']; ?></td>
+                                        <td><?php echo $mostrar2['Gasto_Salud']; ?></td>
+                                        <td><?php echo $mostrar2['Gasto_Vivienda']; ?></td>
+                                        <td><?php echo $mostrar2['Gasto_Empleo']; ?></td>
+                                    </tr>
+                                    <?php } ?>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+
+                           <div class="card mb-4">
+                            <div class="card-header">
+                                <i class="fas fa-table mr-1"></i>
+                                  Ingresos dependientes e independientes durante el 2017
+                            </div>
+                            <div class="card-body">
+                                <div class="table-responsive">
+                                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                     <tr>
+                                       <td>Mes</td>
+                                       <td>Ingreso empleo dependiente</td>
+                                        <td>Ingreso empleo independiente</td>
+                                         <td>Totales</td>
+                                          <td>Promedio de ingresos</td>
+                                    </tr>
+
+                                    <?php
+
+                                        foreach($Resultado3 as $mostrar)
+                                        {
+                                    ?>
+                                    <tr>
+                                        <td><?php echo $mostrar['mesl']; ?></td>
+                                        <td><?php echo $mostrar['IngresoEmpleoDep']; ?></td>
+                                        <td><?php echo $mostrar['IngresoEmpleoIndep']; ?></td>
+                                        <td><?php echo $mostrar['Totales']; ?></td>
+                                        <td><?php echo $mostrar['Promedio_Ingresos']; ?></td>
+                                    </tr>
+                                    <?php } ?>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+
+
+                           <div class="card mb-4">
+                            <div class="card-header">
+                                <i class="fas fa-table mr-1"></i>
+                                  Ingresos dependientes e independientes durante el 2018
+                            </div>
+                            <div class="card-body">
+                                <div class="table-responsive">
+                                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                    <tr>
+                                       	<td>Mes</td>
+                                       	<td>Ingreso empleo dependiente</td>
+                                       	<td>Ingreso empleo independiente</td>
+                                        <td>Totales</td>
+                                        <td>Promedio de ingresos</td>
+                                    </tr>
+
+                                    <?php
+
+                                        foreach($Resultado4 as $mostrar)
+                                        {
+                                    ?>
+                                    <tr>
+										<td><?php echo $mostrar['mesl']; ?></td>
+                                        <td><?php echo $mostrar['IngresoEmpleoDep']; ?></td>
+                                        <td><?php echo $mostrar['IngresoEmpleoIndep']; ?></td>
+                                        <td><?php echo $mostrar['Totales']; ?></td>
+                                        <td><?php echo $mostrar['Promedio_Ingresos']; ?></td>
+                                    </tr>
+                                    <?php } ?>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+                <footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid">
+                        <div class="d-flex align-items-center justify-content-between small">
+                            
+                        </div>
+                    </div>
+                </footer>
+            </div>
+        </div>
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="js/scripts.js"></script>
+        <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
+        <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
+        <script src="assets/demo/datatables-demo.js"></script>
+    </body>
+</html>

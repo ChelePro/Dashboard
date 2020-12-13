41eb1892483bc9b0f97288c72657a3fa1dedb3b1
Ricardo Elías <ricardoportillo14@gmail.com>
2020-12-13 10:01:53 -0600

    Conexion

---
+++ model/conexion.php
@@ -0,0 +1,11 @@
+<?php
+
+	$Servidor = "localhost";
+	$Usuario = "root";
+	$Contrasena = "";
+	$BD = "datos_ehpm";
+	$Conexion;
+
+	$conexion = mysqli_connect($servidor, $usuario, $contrasena, $basedatos);
+
+?> 

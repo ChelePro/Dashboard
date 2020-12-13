48aeda67cf559757d14251645d6a93415fa84da2
Ricardo Elías <ricardoportillo14@gmail.com>
2020-12-13 10:23:45 -0600

    Consultas añadidas
    Consultas añadidas a la bd

--- model/ConsultasBD.php
+++ model/ConsultasBD.php
@@ -17,8 +17,23 @@
                     round(avg(GastoHogarEmp),2) as Gasto_Empleo from fact_monetario
 	                    inner join dim_empleo on dim_empleo.dim_empleo_id = fact_monetario.dim_empleo_id
 		                    inner join dim_tiempo on dim_tiempo.dim_tiempo_id = fact_monetario.dim_tiempo_id
-			                    where dim_tiempo.anio = 2018 group by dim_empleo.GrupoOcupacional order by Promedio_Gastos desc"
+			                    where dim_tiempo.anio = 2018 group by dim_empleo.GrupoOcupacional order by Promedio_Gastos desc";
 
-     $ActLaborales2018 = mysqli_query($Conexion, $Consulta2);       
+     $ActLaborales2018 = mysqli_query($Conexion, $Consulta2);     
 
+	 $Consulta3 = "select mes, round(sum(IngresoEmpleoDep), 2) as IngresoEmpleoDep, round(sum(IngresoEmpleoIndep),2) as IngresoEmpeloIndep,
+					round(sum(IngresoEmpleoDep + IngresoEmpleoIndep), 2) as Totales, round(avg(IngresoTotalHogar), 2) as Promedio_Ingresos
+						from fact_monetario
+    						inner join dim_tiempo on dim_tiempo.dim_tiempo_id = fact_monetario.dim_tiempo_id
+								where dim_tiempo.anio = 2017 group by dim_tiempo.mes order by Promedio_Ingresos desc";
+
+	$IngresosMes2017 = mysqli_query($Conexion, $Consulta3);
+
+	$Consulta4 = "select mes, round(sum(IngresoEmpleoDep), 2) as IngresoEmpleoDep, round(sum(IngresoEmpleoIndep),2) as IngresoEmpeloIndep,
+					round(sum(IngresoEmpleoDep + IngresoEmpleoIndep), 2) as Totales, round(avg(IngresoTotalHogar), 2) as Promedio_Ingresos
+						from fact_monetario
+ 						inner join dim_tiempo on dim_tiempo.dim_tiempo_id = fact_monetario.dim_tiempo_id
+							where dim_tiempo.anio = 2018 group by dim_tiempo.mes order by Promedio_Ingresos desc";
+
+	$IngresosMes2018 = mysqli_query($Conexion, $Consulta4);
 ?>
